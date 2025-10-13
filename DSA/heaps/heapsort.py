8c4e479c8c4d3f8c82fd8fcebfe8184bf1c4b2fb
Adam <adametyang69@icloud.com>
2025-10-14 02:09:03 +0300

    Sorted

--- DSA/heaps/heapsort.py
+++ DSA/heaps/heapsort.py
@@ -1,13 +1,37 @@
-def max_heapify(i):
-    pass
 
+#Build the heap data structure(Max heap)
+def build_heap(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
 
-def build_heap(arr):
+    #Perform swaps to maintain the heap property when building the heap
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        build_heap(arr,n,largest)
+
+def heapSort(arr) -> list :
     n = len(arr)
+
     for i in range(n//2-1,-1,-1):
-        max_heapify(i)
+        build_heap(arr, n, i)
+
+    for i in range(n-1,0,-1):
+        arr[i], arr[0] = arr[0], arr[i]
+        build_heap(arr, i , 0)
+
+    return arr
 
 
 
 if __name__ == "__main__":
-    pass
+    arr = [4,10,3,5,1]
+    heapSort(arr)
+    print(arr)
+
